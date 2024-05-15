b8ad4ce886da08c719676f706eba565267238d6c
jvncode <jvncode@gmail.com>
2024-05-15 19:02:05 +0200

    Refactor form and update translations

--- src/apps/entities/forms.py
+++ src/apps/entities/forms.py
@@ -1,9 +1,26 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from apps.entities.models import Entity
+from project.fields.flowbite import (
+    FormCharField,
+    FormEmailField,
+    FormIntegerField,
+    FormBooleanField,
+)
 
 
 class EntityForm(forms.ModelForm):
+    email = FormEmailField(label=_("Email"))
+    fiscal_name = FormCharField(label=_("Fiscal name"))
+    nif = FormCharField(label=_("NIF"))
+    town = FormCharField(label=_("Towm"))
+    postal_code = FormIntegerField(label=_("Postal code"))
+    address = FormCharField(label=_("Address"))
+    country = FormCharField(label=_("Country"))
+    person_responsible = FormCharField(label=_("Person responsible"))
+    is_resident = FormBooleanField(label=_("Resident"))
+
     class Meta:
         model = Entity
         fields = "__all__"

b707b1df07b9d85ca14ea3e7c649b42246f1463d
Elías <eliashz@gmail.com>
2024-09-10 13:54:40 +0200

    Change htmx tags

--- src/apps/reservations/forms.py
+++ src/apps/reservations/forms.py
@@ -86,15 +86,7 @@ class ReservationForm(forms.ModelForm):
         label=_("Assitants"),
         widget=forms.NumberInput(
             attrs={
-                "data-input-counter": "",
-                "aria-describedby": "helper-text-explanation",
-                "class": "form-control bg-gray-50 border-x-0 border-gray-300 "
-                "h-11 text-center text-gray-900 text-sm block w-full "
-                "py-2.5 focus:ring-primary-500 focus:border-primary-500 "
-                "dark:bg-gray-700 "
-                "dark:border-gray-600 dark:placeholder-gray-400 "
-                "dark:text-white dark:focus:ring-primary-500 "
-                "dark:focus:border-primary-500",
+                "class": "form-control",
                 "autocomplete": True,
             }
         ),

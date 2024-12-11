43aa34bf7b17b03b73454accbacecf439653ddf2
eliashz <eliashz@gmail.com>
2024-12-11 10:58:50 +0100

    Half day to 13:00

--- src/apps/reservations/constants.py
+++ src/apps/reservations/constants.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 START_TIME = time(8, 0)
 START_TIME_PLUS_ONE = time(9, 0)
-HALF_TIME = time(14, 0)
+HALF_TIME = time(13, 0)
 END_TIME = time(18, 0)
 END_TIME_MINUS_ONE = time(17, 0)
 # Discounts

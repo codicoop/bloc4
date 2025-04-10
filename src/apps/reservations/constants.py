26c0a830bd2c32ea2410051dfed48ceb5396b750
Pere Picornell <pere@perepicornell.com>
2025-04-10 16:11:50 +0200

    Canvis de noms a tipus d'entitat + afegir la que faltava amb el descompte del 15%.

--- src/apps/reservations/constants.py
+++ src/apps/reservations/constants.py
@@ -9,9 +9,10 @@ HALF_TIME = time(13, 0)
 END_TIME = time(20, 0)
 END_TIME_MINUS_ONE = time(19, 0)
 # Discounts
-GENERAL_DISCOUNT = Decimal(0)
 HOSTED_DISCOUNT = Decimal(-0.4)
 BLOC4_DISCOUNT = Decimal(-0.5)
+GENERAL_DISCOUNT = Decimal(0)
+FEDERATED_DISCOUNT = Decimal(-0.15)
 OUTSIDE_DISCOUNT = Decimal(0.15)
 # Months
 MONTHS = {
